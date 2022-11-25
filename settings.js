6249fbc66cc9df968b4a10a8dd17d4fa86e494c5
ghwhatsappbot <119060090+ghwhatsappbot@users.noreply.github.com>
2022-11-25 19:58:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['GIHAN']
+global.premium = ['GH WHATSAPP BOT']
+global.ownernomer = '94783955844'
+global.ownername = '🐦GH WHATSAPP BOT'
+global.botname = 'GH WHATSAPP BOT'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'Gihan Whatsapp Bot'
+global.author = '🐦GH WHATSAPP BOT'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
